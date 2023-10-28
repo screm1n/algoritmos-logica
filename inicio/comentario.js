d6fc130fc9eb184a7628b4fd734abf1bf543ea79
screm1n <screminoctavio@gmail.com>
2023-10-28 20:10:59 -0300

    exercises done

---
+++ inicio/comentario.js
@@ -0,0 +1,10 @@
+// console.log é usado pra exibir algo na tela do computador
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+
+/*
+    Comentário
+    de múltiplas
+    linhas
+*/
