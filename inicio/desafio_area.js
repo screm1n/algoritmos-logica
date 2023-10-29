e258677d4481b4e05b2f0c56702f9a78ce2f7d8f
screm1n <screminoctavio@gmail.com>
2023-10-29 17:07:20 -0300

    desafios concluídos

---
+++ inicio/desafio_area.js
@@ -0,0 +1,14 @@
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.14;
+console.log("A área da circunferência é:" + PI * raio * raio)
+
+// ou
+
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da area é " + areaCirc + "m2.");
+
+// pi do js
+console.log(Math.PI);
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,10 @@
+// trocar essas variaveis
+
+let a = 7
+let b = 94
+
+a = b
+b = 7
+// depois da troca a = 94 e b = 7
+console.log(a)
+console.log(b)
