657948577e3146fa3e88bb194131e031b0611e4e
screm1n <screminoctavio@gmail.com>
2023-11-01 21:02:49 -0300

    seçao if concluida

---
+++ controle/desafio_if.js
@@ -0,0 +1,16 @@
+const nota = 0;
+
+if (nota >= 9 && nota <= 10)
+    console.log('Você tirou nota A!');
+
+if (nota >= 7 && nota < 9)
+    console.log('Você tirou nota B!');
+
+if (nota >= 5 && nota < 7)
+    console.log('Você tirou nota C!');
+
+if (nota >= 4.5 && nota < 5)
+    console.log('Você tirou nota D!');
+
+if (nota <= 4.4 && nota >= 0)
+    console.log('Você tirou nota F!');
---
+++ controle/if_3.js
@@ -0,0 +1,8 @@
+const nota = 9;
+
+if(nota >= 9) {
+    console.log('Você foi parar o quadro de honra!');
+    console.log('Parabéns!');
+}
+
+console.log('Fim')
