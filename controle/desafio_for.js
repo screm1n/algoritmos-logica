2a988295cc13aa122531ecd22e5a3b0fa20eadf3
screm1n <screminoctavio@gmail.com>
2023-11-04 20:38:32 -0300

    desafio for

---
+++ controle/desafio_for.js
@@ -0,0 +1,40 @@
+/*
+#
+##
+###
+####
+#####
+######
+*/
+
+let linha = '';
+
+for(let i = 1; i <= 1; i++) {
+    linha+= '#';
+    console.log('#');
+}
+
+for(let i = 1; i <= 1; i++) {
+    linha+= '#';
+    console.log('##');
+}
+
+for(let i = 1; i <= 1; i++) {
+    linha+= '#';
+    console.log('###');
+}
+
+for(let i = 1; i <= 1; i++) {
+    linha+= '#';
+    console.log('####');
+}
+
+for(let i = 1; i <= 1; i++) {
+    linha+= '#';
+    console.log('#####');
+}
+
+for(let i = 1; i <= 1; i++) {
+    linha+= '#';
+    console.log('######');
+}
