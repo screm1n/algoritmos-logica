dbb16e3fb712e89dcf07ca4ce901a8561ef52a1b
screm1n <screminoctavio@gmail.com>
2023-11-08 11:26:11 -0300

    aprendendo arrays

---
+++ arrays/basico_1.js
@@ -0,0 +1,10 @@
+// array        0    1   2    3   4    5
+const notas = [7.2, 6.7, 10, 9.2, 7.9, 5];
+
+console.log(notas);
+console.log(notas[3]);
+console.log(notas[5]);
+console.log(notas[6]);
+
+notas[4] = 8.5; // modifiquei o valor de um dos elementos (o de indice 4)
+console.log(notas)
