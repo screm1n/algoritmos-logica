a45857aac60c39a2e2b6483d1ab1fb4b7d514e27
screm1n <screminoctavio@gmail.com>
2023-11-09 13:31:31 -0300

    exercicios concluidos

---
+++ exercicios/ex_1.js
@@ -0,0 +1,14 @@
+function cumprimentar(nome) {
+    return "Olá, " + nome + "!";
+}
+console.log(cumprimentar("Leo"));
+console.log(cumprimentar("Ana"));
+
+
+// ou
+
+function cumprimentar1(nome) {
+    return `Olá, ${nome}!`
+}
+
+console.log(cumprimentar1("Luiz"))
---
+++ exercicios/ex_2.js
@@ -0,0 +1,8 @@
+// Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dia
+function converterIdadeEmAnosParaDias(idade) {
+    const diasDoAno = 365
+
+    return idade * diasDoAno;
+}
+
+console.log(converterIdadeEmAnosParaDias(22));
